ab385a7a40d03164afb8750fb606c926873ec74f
Zaki Dzulfikar <berckjack@gmail.com>
2025-06-21 21:33:06 +0700

    fix: fixing project structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import Navigation from '../components/navigation/index.svelte';
+	import Navigation from '../components/Navigation/index.svelte';
 	import Footer from '../components/footer/index.svelte';
 
 	let { children } = $props();
