0dfe249ccc9c58c9f0aca9dee5239aa5b4b173d0
Zaki Dzulfikar <berckjack@gmail.com>
2025-07-06 22:01:25 +0700

    configure: configuring svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	errorHandling: {
+		errorPage: '/src/routes/%2Berror.svelte'
+	}
 };
 
 export default config;
