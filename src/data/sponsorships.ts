628aa7c0e1948eed28f650fcd439faf77f449065
Zaki Dzulfikar <berckjack@gmail.com>
2025-07-29 14:54:46 +0700

    docs: adding some sponsorship

--- src/data/sponsorships.ts
+++ src/data/sponsorships.ts
@@ -44,13 +44,78 @@ const sponsorships: {
 		size: 'md' // Aslinya 'sm', but adjusted for visibility
 	},
 	{
+		name: 'ZIFT Indonesia',
+		logo: '/img/sponsorships/zift.png',
+		size: 'xs'
+	},
+	{
 		name: 'Hydro Coco',
 		logo: '/img/sponsorships/hydro-coco.png',
 		size: 'xs'
 	},
 	{
-		name: 'ZIFT Indonesia',
-		logo: '/img/sponsorships/zift.png',
+		name: 'Artivity',
+		logo: '/img/sponsorships/artivity.png',
+		size: 'xs'
+	},
+	{
+		name: 'Hangout',
+		logo: '/img/sponsorships/hangout.jpg',
+		size: 'xs'
+	},
+	{
+		name: 'Arasta Alpha',
+		logo: '/img/sponsorships/arasta-alpha.png',
+		size: 'md'
+	},
+	{
+		name: 'Bento Kopi',
+		logo: '/img/sponsorships/bento-kopi.png',
+		size: 'xs'
+	},
+	{
+		name: 'Colony',
+		logo: '/img/sponsorships/colony.png',
+		size: 'xs'
+	},
+	{
+		name: 'Sekala',
+		logo: '/img/sponsorships/sekala.jpg',
+		size: 'xs'
+	},
+	{
+		name: 'Grey',
+		logo: '/img/sponsorships/grey.jpg',
+		size: 'xs'
+	},
+	{
+		name: 'Taman Coffee',
+		logo: '/img/sponsorships/taman-coffee.jpg',
+		size: 'xs'
+	},
+	{
+		name: 'Teras Kita',
+		logo: '/img/sponsorships/teras-kita.jpg',
+		size: 'xs'
+	},
+	{
+		name: 'KAI',
+		logo: '/img/sponsorships/kai.png',
+		size: 'md'
+	},
+	{
+		name: 'Ora Umum',
+		logo: '/img/sponsorships/ora-umum.png',
+		size: 'xs'
+	},
+	{
+		name: 'Advo Cafe',
+		logo: '/img/sponsorships/advo-cafe.jpg',
+		size: 'xs'
+	},
+	{
+		name: 'Meatboss',
+		logo: '/img/sponsorships/meatboss.png',
 		size: 'xs'
 	}
 ];
